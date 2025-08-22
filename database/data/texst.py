eb6a70cf83a164ca72fe31a16a231d7df8760256
Andre <andrey.bone2@gmail.com>
2025-08-22 18:29:54 +0200

    Fix data/texts.py name and content

---
+++ database/data/texst.py
@@ -0,0 +1,9 @@
+texts = {
+    "choose_language": {
+        "en": "Please choose your language:",
+    },
+    "language_saved": {
+        "en": "Language set to English!",
+        "sr": "Jezik je postavljen na srpski!"
+    }
+}
